a72c81dffb2d701efd959071e5ea8474b0f15632
Kristofer <kroldan@ford.com>
2022-04-11 15:12:24 -0400

    Add logValues function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function logValues(){
-  // Your code here
+  const firstName = document.querySelector("#first-name");
+  const lastName = document.querySelector("#last-name");
+  const emailAddress = document.querySelector("#email");
+  console.log(`${firstName.value}`)
+  console.log(`${lastName.value}`)
+  console.log(`${emailAddress.value}`)
 }
+
+const button = document.querySelector("button");
